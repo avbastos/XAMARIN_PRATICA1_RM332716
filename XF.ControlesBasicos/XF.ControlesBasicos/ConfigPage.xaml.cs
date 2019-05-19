49fb05e4f4a1a52b0d6ac3622e9674cc928d05bc
Alex Bastos <vbastos.alex@gmail.com>
2019-05-19 20:05:28 -0300

    Add project files.

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/ConfigPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.ControlesBasicos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConfigPage : ContentPage
+	{
+       
+        public ConfigPage ()
+		{
+			InitializeComponent ();
+            txtEmail.IsEnabled = swtSendEmail.On;
+        }
+
+        private void SwitchCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            txtEmail.IsEnabled = swtSendEmail.On;
+            if (!swtSendEmail.On)
+            {
+                txtEmail.Text = null;
+            }
+        }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ControlesBasicos
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private Configuracao _vmConfig;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+
+            if (_vmConfig != null && _vmConfig.SendEmail)
+            {
+                DisplayAlert("Atenção", $"Email enviado para {_vmConfig.Email}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Atenção"
+                       , "E-mail não autorizado!"
+                       , "OK");
+            }
+        }
+
+        private void BtnConfig_Clicked(object sender, EventArgs e)
+        {
+            if (_vmConfig == null)
+                _vmConfig = new Configuracao();
+
+            Navigation.PushAsync(new ConfigPage() { BindingContext = _vmConfig });
+        }
+    }
+}
